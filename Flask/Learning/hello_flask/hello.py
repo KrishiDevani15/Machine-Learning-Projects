f0d0aa93d420af8ea08d83cf582f67c08a76f820
KrishiDevani15 <krishiminergate@gmail.com>
2024-02-23 13:06:52 +0530

    Flask Function is completed

--- Flask/Learning/hello_flask/hello.py
+++ Flask/Learning/hello_flask/hello.py
@@ -1,7 +1,9 @@
 from flask import Flask
-app=Flask(__name__)
+app=Flask(__name__) #Special Attribute __name__ --> the name of class,functions,methods,descriptor or generator instance.
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
 
+if __name__ == '__main__':
+    app.run() # du to this we dont need to write set FLASK_APP = hello.py, flask run ---> Instead directly write hello.py in cmd
